916093ee9afe0db7573ccbb172b6cc8f249f3f9c
ExtraPolite <imextrapolite2@gmail.com>
2024-05-20 15:10:59 -0400

    get user input to log

--- library.js
+++ library.js
@@ -1,35 +1,47 @@
 const myLibrary = [];
 let read = false;
 
-function Book(title, author, numOfPages) {
+function Book(title, author, numOfPages, read) {
   this.title = title;
   this.author = author;
   this.numOfPages = numOfPages;
-}
-
-Book.prototype.ifRead = function() {
-  const yesRead = document.getElementById('read');
-  const noRead = document.getElementById('not-read');
-
-  yesRead.addEventListener('click', () => {
-    read = true;
-    this.read = read;
-  });
-
-  noRead.addEventListener('click', () => {
-    read = false;
-    this.read = read
-  })
+  this.read = read;
 }
 
 function addBookToLibrary() {
   const title = document.getElementById('title');
   const author = document.getElementById('author');
   const numOfPages = document.getElementById('num-of-pages');
+  const yesRead = document.getElementById('read');
+  const noRead = document.getElementById('not-read');
+  const confirm = document.getElementById('confirm');
 
-  const newBook = new Book(title.value, author.value, numOfPages.value);
+  yesRead.addEventListener('click', () => {
+    read = true;
+    const newBook = new Book(title.value, author.value, numOfPages.value, read);
 
-  console.log(`TITLE: ${newBook.title}, AUTHOR: ${newBook.author}, NUM OF PAGES: ${newBook.numOfPages}`)
+    confirm.addEventListener('click', () => {
+      whenConfirm(newBook.title, newBook.author, newBook.numOfPages, newBook.read);
+
+      title.value = '';
+      author.value = '';
+      numOfPages.value = '';
+      read = false;
+    })
+  });
+
+  noRead.addEventListener('click', () => {
+    read = false;
+    const newBook = new Book(title.value, author.value, numOfPages.value, read);
+
+    confirm.addEventListener('click', () => {
+      whenConfirm(newBook.title, newBook.author, newBook.numOfPages, newBook.read);
+    })
+  });
+}
+
+function whenConfirm(title, author, numOfPages, read) {
+  console.log(`${title}, ${author}, ${numOfPages}, ${read}`);
 }
 
 function displayBooks() {
@@ -64,3 +76,4 @@ function closeModal() {
 openModal();
 closeModal();
 displayBooks();
+addBookToLibrary();
